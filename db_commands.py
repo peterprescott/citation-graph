60cda3319b47eee7e201ac119079b1537fee20dd
Peter Prescott <peter@prescott.ws>
2019-11-21 00:00:55 +0000

    put functions in classes

--- db_commands.py
+++ db_commands.py
@@ -1,144 +1,141 @@
 import sqlite3  #db functionality ~ https://docs.python.org/3/library/sqlite3.html
 import string
 
-db = 'citation_graph.db'
+class Query():
 
-def connect(db):
-    """
-    Decorates function with database connection.
-    
-    Args:
-        db (string): filename of database (will be created if non-existent).
-    """
-    def wrapper(func):
-        def connected_function(*args, **kwargs):
-            """See source code. 
-            Sphinx doesn't straightforwardly autodocument decorated functions..."""
+    def __init__(self, db_filename):
+        self.db = db_filename
+        
+    def open(self):
+        self.conn = sqlite3.connect(self.db)
+        self.c = self.conn.cursor()
+        
+    def close(self):
+        self.conn.commit()
+        self.conn.close()
+                
+    def test_connection(self):
+        """Shows that tests are successfully importing db_commands"""
+        print("connected")
+        return "connected"
 
-            conn = sqlite3.connect(db)
-            c = conn.cursor()
-            print(f'connected to {db}.')
-            process = func(*args, **kwargs, c=c)
-            conn.commit()
-            conn.close()
-            print('connection closed.')
-            return process
-        return connected_function
-    return wrapper
-    
-def test_connection():
-    """Shows that tests are successfully importing db_commands"""
-    print("connected")
-    return "connected"
+    def create_table(self, table, columns):
+        """
+        Creates a table.
+        
+        Args:
+            table (string)
+            columns (tuple of strings)
+            c: database connection.
+        """
 
-@connect(db)
-def create_table(table, columns, c):
-    """
-    Creates a table.
+        self.open()
+        print('creating table')
+        self.c.execute(f'''CREATE TABLE {table} {columns}''')
+        print('created table')
+        self.close()
+        
     
-    Args:
-        table (string)
-        columns (tuple of strings)
-        c: database connection.
-    """
-    print('creating table')
-    c.execute(f'''CREATE TABLE {table}
-             {columns}''')
-    print('created table')
+    def drop_table(self, table):
+        """
+        Drops table.
+        
+        Args:
+            table (string)
+            c: database connection.
+        """
+        
+        self.open()
+        print(f"dropping table '{table}'")
+        self.c.execute(f'''DROP TABLE {table}''')
+        self.conn.close()
 
-@connect(db)
-def drop_table(table, c):
-    """
-    Drops table.
-    
-    Args:
-        table (string)
-        c: database connection.
-    """
-    print(f"dropping table '{table}'")
-    c.execute(f'''DROP TABLE {table}''')
+    def save_row_to_table(self, table, row, allow_duplicate=False):
+        """
+        Saves row of data to table.
+        
+        Args:
+            data (list)
+        """
+        
+        self.open()
+        print('...saving row to table...')
+        
+        if allow_duplicate == False:
+            # check if duplicate already exists
+            if self.search(table, 'key', row[0]):
+                print(f'ERROR in trying to save {row} to {table}: row already added!')
+                return {'status':'error','error':'key already exists'}
+        
+        question_marks = ('?,' * len(row))[0:-1] # slice to remove trailing comma
+        self.c.executemany(f"INSERT INTO {table} VALUES ({question_marks});", (row,))
+        self.close()
+        print(f"Row saved to table '{table}' in database '{self.db}': {row}")
+        return {'status':'successful'}
 
-@connect(db)
-def save_row_to_table(table, row, c, allow_duplicate=False):
-    """
-    Saves row of data to table.
-    
-    Args:
-        data (list)
-    """
-    print('...saving row to table...')
-    
-    if allow_duplicate == False:
-        # check if duplicate already exists
-        if search(table, 'key', row[0]):
-            print(f'ERROR in trying to save {row} to {table}: row already added!')
-            return {'status':'error','error':'key already exists'}
-    
-    question_marks = ('?,' * len(row))[0:-1] # slice to remove trailing comma
-    c.executemany(f"INSERT INTO {table} VALUES ({question_marks});", (row,))
-    print(f"Row saved to table '{table}' in database '{db}': {row}")
-    return {'status':'successful'}
+    def search(self, table, column, value, with_rowid=False):
+        """
+        Searches column in table for specified value.
+        
+        Args:
+            table (string)
+            column (string)
+            value
+            c: database connection
+            with_rowid (Boolean)
+        """
+        
+        self.open()
+        print(f'searching for value "{value}" in column "{column}" of table "{table}"')
+        if with_rowid==True:
+            self.c.execute(f"SELECT rowid, * FROM {table} WHERE {column}=?", (value,))
+        else:
+            self.c.execute(f"SELECT * FROM {table} WHERE {column}=?", (value,))
+        search_results = self.c.fetchall()
+        print(search_results)
+        return search_results
 
-@connect(db)
-def search(table, column, value, c, with_rowid=False):
-    """
-    Searches column in table for specified value.
-    
-    Args:
-        table (string)
-        column (string)
-        value
-        c: database connection
-        with_rowid (Boolean)
-    """
-    print(f'searching for value "{value}" in column "{column}" of table "{table}"')
-    if with_rowid==True:
-        c.execute(f"SELECT rowid, * FROM {table} WHERE {column}=?", (value,))
-    else:
-        c.execute(f"SELECT * FROM {table} WHERE {column}=?", (value,))
-    search_results = c.fetchall()
-    print(search_results)
-    return search_results
+    def remove_row(self, table, rowid):
+        """
+        Removes specified row from table.
+        
+        Args:
+            table (string)
+            rowid (int)
+            c: database connection
+        """
+        
+        self.open()
+        print('removing row {rowid} from {table}')
+        self.c.execute(f"DELETE FROM {table} WHERE rowid=?", (rowid,))
+        self.close()
 
-@connect(db)
-def remove_row(table, rowid, c):
-    """
-    Removes specified row from table.
-    
-    Args:
-        table (string)
-        rowid (int)
-        c: database connection
-    """
-    print('removing row {rowid} from {table}')
-    c.execute(f"DELETE FROM {table} WHERE rowid=?", (rowid,))
-
-@connect(db)
-def remove_duplicate_rows(table, repeated_value, c, column='key'):
-    """
-    Removes rows with specified duplicate value.
-    
-    Args:
-        table (string)
-        repeated_value
-        c: database connection
-        column (string): default is 'key' which will only remove duplicate rows,
-                        but any column could be specified to remove all rows 
-                        with duplicate values.
-    """
-    while len(search(table, column, repeated_value, with_rowid=True)) > 1:
-        remove_row(table, search(table, column, repeated_value, with_rowid=True)[1][0])
+    def remove_duplicate_rows(self, table, repeated_value, all_but=1, column='key'):
+        """
+        Removes rows with specified duplicate value.
+        
+        Args:
+            table (string)
+            repeated_value
+            c: database connection
+            column (string): default is 'key' which will only remove duplicate rows,
+                            but any column could be specified to remove all rows 
+                            with duplicate values.
+        """
+        
+        while len(self.search(table, column, repeated_value, with_rowid=True)) > all_but:
+            self.remove_row(table, self.search(table, column, repeated_value, with_rowid=True)[1][0])
 
 
 
 if __name__ == '__main__':
     
-    text_columns = ('key', 'type')
-    book_columns = ('key', 'publication_year', 'title', 'publisher', 'location', 'number_of_pages')
-    chapter_columns = ('key', 'title', 'pages', 'book_key')
-    article_columns = ('key', 'publication_year', 'title', 'journal', 'volume', 'edition', 'pages')
-    creator_columns = ('key', 'surname', 'initial', 'year_of_birth', 'year_of_death')
-    text_creator_columns = ('key', 'creator_key', 'creator_ordinal')
+    text_columns = ('key', 'publication_year', 'title', 'type')
+    book_columns = ('key', 'publisher', 'location', 'number_of_pages')
+    chapter_columns = ('key', 'pages', 'book_key')
+    article_columns = ('key', 'journal', 'volume', 'edition', 'pages')
+    creator_columns = ('key', 'surname', 'initial')
+    text_creator_columns = ('key', 'text_key', 'creator_key', 'creator_role', 'creator_ordinal')
     citation_columns = ('key', 'citing_key', 'cited_key')
     
     # ~ create_table('texts', text_columns)
--- tests/__init__.py
+++ tests/__init__.py
@@ -6,206 +6,199 @@ import sqlite3                  # for testing database
 import os                       # for navigating between folders
 import sys
 
+class Test():
+    
+    def __init__(self):
+        self.tests = 0
+        self.success = 0
+        with open(os.path.join(sys.path[0], 'logs.txt'), 'a') as log:
+            now = datetime.now()
+            log.write("\n" + now.strftime("%d/%m/%Y %H:%M:%S"))
+            log.write(self.logic())
+            log.write(self.flask_app("localhost:5000/test", "Running!"))
+            log.write(self.db_commands(os.path.join(sys.path[0],'..','db_commands.py')))
+            # ~ log.write(self.lit_classes())
+            # ~ log.write(self.bib_reader())
+            # ~ log.write(self.pdf_reader())
+            # ~ log.write(self.api_interactions())
+            # ~ log.write(self.jamstack_gui())
+            # ~ log.write(self.web_scraper())
+            summary = f"\nRan {self.tests} tests: {self.success}/{self.tests} were successful."
+            print(summary)
+            log.write(summary)
+            log.write("\n")
+        
+    def logic(self):
+        
+        self.tests += 1
+        
+        try:
+            assert True == True
+            self.success += 1
+            result = "\nTest Successful: Logic working as expected."
+        except:
+            result = "\nTest Failed: Logic not working as expected."
+        
+        print(result)
+        return result
 
-test_count = 0
-success_count = 0
+
+    def flask_app(self, page_location, confirmation):
+        """
+        Tests that Flask App is running as expected.
+
+        Uses Selenium Webdriver to check Flask App is running as expected.
+
+        Args:
+            URL (string): the address where the Flask App is running.
+            page_title (string): the title of the webpage,
+                as it should be defined by <title> tags.
+        """
+
+        self.tests += 1
+
+        driver = webdriver.Chrome(os.path.join(sys.path[0], '..', 'chromedriver.exe'))
+        driver.get(page_location)
+        print(driver)
+        if driver.title == confirmation:
+            self.success += 1
+            result = "\nTest Successful: Flask App running as expected."
+        else:
+            result = """\nTest Failed: Flask App not running as expected. 
+                    (It may not be broken -- you need to run it explicitly.)"""
+        print(result)
+        return result
+
+    def db_commands(self, file):
+        """
+        Tests that database commands from ../db_commands.py are working as expected.
+        
+        Args:
+            file (string): file location for db_commands.py
+        """ 
+        
+        sys.path.append(os.path.dirname(os.path.expanduser(file)))
+        import db_commands as db
+        
+        self.tests += 1
+        
+        details = ""
+
+        try:
+            q = db.Query(os.path.join(sys.path[0], '..', 'citation_graph.db'))
+            assert q.test_connection() == "connected"
+            details += '\n>>>Connection working'
+            q.create_table('test_table', ('key', 'other_column'))
+            details += '\n>>>create_table() working'
+            q.save_row_to_table('test_table', ('test_key', 'testing_testing'))
+            details += '\n>>>save_row_to_table() working'
+            assert q.search('test_table', 'key', 'test_key')[0] == ('test_key', 'testing_testing')
+            details += '\n>>>search() working'
+            q.remove_row('test_table', 1)
+            details += '\n>>>remove_row() working'
+            assert len(q.search('test_table', 'key', 'test_key')) == 0
+            q.save_row_to_table('test_table', ('test_key', 'testing_testing'), allow_duplicate=True)
+            q.save_row_to_table('test_table', ('test_key', 'testing_testing'), allow_duplicate=True)
+            assert len(q.search('test_table', 'key', 'test_key')) == 2
+            details += '\n>>>testing remove_duplicate_rows()'
+            q.remove_duplicate_rows('test_table', 'test_key')
+            assert len(q.search('test_table', 'key', 'test_key')) == 1
+            details += '\n>>>remove_duplicate_rows() working'
+            q.drop_table('test_table')
+            details += '\n>>>drop_table() working'
+            self.success += 1
+            result = "\nTest Successful: Database Commands working as expected."
+        except:
+            result = "\nTest Failed: Database Commands not working as expected."
+            result += details
+        print(result)
+        return result
+
+    def lit_classes(self):
+        
+        self.tests += 1
+        
+        try:
+            assert True == True
+            self.success += 1
+            result = "\nTest Successful: Literature Classes working as expected."
+        except:
+            result = "\nTest Failed: Literature Classes not working as expected."
+        
+        print(result)
+        return result
+        
+    def bib_reader(self):
+        
+        self.tests += 1
+        
+        try:
+            assert True == True
+            self.success += 1
+            result = "\nTest Successful: .bib Reader working as expected."
+        except:
+            result = "\nTest Failed: .bib Reader not working as expected."
+        
+        print(result)
+        return result
+
+    def pdf_reader(self):
+        
+        selfcount += 1
+        
+        try:
+            assert True == True
+            self.success += 1
+            result = "\nTest Successful: PDF Reader working as expected."
+        except:
+            result = "\nTest Failed: PDF Reader not working as expected."
+        
+        print(result)
+        return result
+        
+    def api_interactions(self):
+        
+        self.tests += 1
+        
+        try:
+            assert True == True
+            self.success += 1
+            result = "\nTest Successful: DOI & OCI API interactions working as expected."
+        except:
+            result = "\nTest Failed: DOI & OCI API interactions not working as expected."
+        
+        print(result)
+        return result
+        
+    def jamstack_gui(self):
+        
+        self.tests += 1
+        
+        try:
+            assert True == True
+            self.success += 1
+            result = "\nTest Successful: JAMstack GUI working as expected."
+        except:
+            result = "\nTest Failed: JAMstack GUI not working as expected."
+        
+        print(result)
+        return result
+        
+    def web_scraper(self):
+        
+        self.tests += 1
+        
+        try:
+            assert True == True
+            self.success += 1
+            result = "\nTest Successful: Web Scraper working as expected."
+        except:
+            result = "\nTest Failed: Web Scraper not working as expected."
+        
+        print(result)
+        return result
 
 
-def test_flask_app(page_location, confirmation):
-    """
-    Tests that Flask App is running as expected.
-
-    Uses Selenium Webdriver to check Flask App is running as expected.
-
-    Args:
-        URL (string): the address where the Flask App is running.
-        page_title (string): the title of the webpage,
-            as it should be defined by <title> tags.
-    """
-
-    global test_count 
-    global success_count
-    test_count += 1
-
-    driver = webdriver.Chrome(os.path.join(sys.path[0], 'chromedriver.exe'))
-    driver.get(page_location)
-    print(driver)
-    if driver.title == confirmation:
-        success_count += 1
-        result = "\nTest Successful: Flask App running as expected."
-    else:
-        result = """\nTest Failed: Flask App not running as expected. 
-                (It may not be broken -- you need to run it explicitly.)"""
-    print(result)
-    return result
-
-def test_db_commands(file):
-    """
-    Tests that database commands from ../db_commands.py are working as expected.
-    
-    Args:
-        file (string): file location for db_commands.py
-    """ 
-    
-    sys.path.append(os.path.dirname(os.path.expanduser(file)))
-    import db_commands as db
-    
-    global test_count
-    global success_count
-    test_count += 1
-    
-    details = ""
-    try:
-        assert db.test_connection() == "connected"
-        details += '\n>>>Connection working'
-        db.create_table('test_table', ('key', 'other_column'))
-        details += '\n>>>create_table() working'
-        db.save_row_to_table('test_table', ('test_key', 'testing_testing'))
-        details += '\n>>>save_row_to_table() working'
-        assert db.search('test_table', 'key', 'test_key')[0] == ('test_key', 'testing_testing')
-        details += '\n>>>search() working'
-        db.remove_row('test_table', 1)
-        details += '\n>>>remove_row() working'
-        assert len(db.search('test_table', 'key', 'test_key')) == 0
-        db.save_row_to_table('test_table', ('test_key', 'testing_testing'), allow_duplicate=True)
-        db.save_row_to_table('test_table', ('test_key', 'testing_testing'), allow_duplicate=True)
-        assert len(db.search('test_table', 'key', 'test_key')) == 2
-        details += '\n>>>testing remove_duplicate_rows()'
-        db.remove_duplicate_rows('test_table', 'test_key')
-        assert len(db.search('test_table', 'key', 'test_key')) == 1
-        details += '\n>>>remove_duplicate_rows() working'
-        db.drop_table('test_table')
-        details += '\n>>>drop_table() working'
-        success_count += 1
-        result = "\nTest Successful: Database Commands working as expected."
-    except:
-        result = "\nTest Failed: Database Commands not working as expected."
-        result += details
-    print(result)
-    return result
-
-def test_lit_classes():
-    global test_count
-    global success_count
-    test_count += 1
-    
-    try:
-        assert True == True
-        success_count += 1
-        result = "\nTest Successful: Literature Classes working as expected."
-    except:
-        result = "\nTest Failed: Literature Classes not working as expected."
-    
-    print(result)
-    return result
-    
-def test_bib_reader():
-    global test_count
-    global success_count
-    test_count += 1
-    
-    try:
-        assert True == True
-        success_count += 1
-        result = "\nTest Successful: .bib Reader working as expected."
-    except:
-        result = "\nTest Failed: .bib Reader not working as expected."
-    
-    print(result)
-    return result
-
-def test_pdf_reader():
-    global test_count
-    global success_count
-    test_count += 1
-    
-    try:
-        assert True == True
-        success_count += 1
-        result = "\nTest Successful: PDF Reader working as expected."
-    except:
-        result = "\nTest Failed: PDF Reader not working as expected."
-    
-    print(result)
-    return result
-    
-def test_api_interactions():
-    global test_count
-    global success_count
-    test_count += 1
-    
-    try:
-        assert True == True
-        success_count += 1
-        result = "\nTest Successful: DOI & OCI API interactions working as expected."
-    except:
-        result = "\nTest Failed: DOI & OCI API interactions not working as expected."
-    
-    print(result)
-    return result
-    
-def test_jamstack_gui():
-    global test_count
-    global success_count
-    test_count += 1
-    
-    try:
-        assert True == True
-        success_count += 1
-        result = "\nTest Successful: JAMstack GUI working as expected."
-    except:
-        result = "\nTest Failed: JAMstack GUI not working as expected."
-    
-    print(result)
-    return result
-    
-def test_web_scraper():
-    global test_count
-    global success_count
-    test_count += 1
-    
-    try:
-        assert True == True
-        success_count += 1
-        result = "\nTest Successful: Web Scraper working as expected."
-    except:
-        result = "\nTest Failed: Web Scraper not working as expected."
-    
-    print(result)
-    return result
-    
-def test_logic():
-    global test_count
-    global success_count
-    test_count += 1
-    
-    try:
-        assert True == True
-        success_count += 1
-        result = "\nTest Successful: Logic working as expected."
-    except:
-        result = "\nTest Failed: Logic not working as expected."
-    
-    print(result)
-    return result
-    
 
 if __name__ == '__main__':
-
-    with open(os.path.join(sys.path[0], 'logs.txt'), 'a') as log:
-        now = datetime.now()
-        log.write("\n" + now.strftime("%d/%m/%Y %H:%M:%S"))
-        log.write(test_logic())
-        log.write(test_flask_app("localhost:5000/test", "Running!"))
-        log.write(test_db_commands(os.path.join(sys.path[0],'..','db_commands.py')))
-        # ~ log.write(test_lit_classes())
-        # ~ log.write(test_bib_reader())
-        # ~ log.write(test_pdf_reader())
-        # ~ log.write(test_api_interactions())
-        # ~ log.write(test_jamstack_gui())
-        # ~ log.write(test_web_scraper())
-        summary = f"\nRan {test_count} tests: {success_count}/{test_count} were successful."
-        print(summary)
-        log.write(summary)
-        log.write("\n")
-        
+    Test()
